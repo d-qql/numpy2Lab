fde73b8a06c34bcfc62706166aadd2f036196bb5
d-qql <petrov.dmitrii@phystehc.edu>
2020-11-20 15:20:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+filenumb = "01"
+file = open("signals/signal" + filenumb + ".dat").read()
+def move_filt(a, n = 10):
+    k = []
+    for i in range(len(a)):
+        if i < n-1:
+            k.append(sum(a[:i+1])/(i+1))
+        else:
+            k.append(sum(a[i-(n-1) : i+1])/10)
+    return np.array(k)
+sig = np.array([float(i) for i in file.split()])
+x = [i for i in range(len(sig))]
+
+plt.subplot(2, 1, 1)
+plt.plot(x, sig)
+plt.title("input signal" + filenumb)
+plt.subplot(2,1,2)
+plt.plot(x, move_filt(sig))
+plt.title("Filtered")
+plt.show()
